ee4ec225a6a5a231669c1b35c843eb241eff8084
oshovskam-sys <oshovskam@gmail.com>
2026-05-02 23:54:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const galleries = [
   { name: "Основна", folder: "images", count: 20 },
   { name: "Стравохід", folder: "esophagus", count: 15 },
   { name: "Шлунок", folder: "stomach", count: 18 },
-  { name: "ДПК", folder: "duodenum", count: 10 }
+  { name: "ДПК", folder: "duodenum", count: 10 },
+  { name: "Патологія", folder: "pathology", count: 25 }
 ];
-{ name: "Патологія", folder: "pathology", count: 25 }
